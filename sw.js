a81280090f923fab9420e5d15282bf1164dfea62
gowthamrs2006-svg <gowthamrs2006@gmail.com>
2026-01-26 00:44:14 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,22 +1,16 @@
-const CACHE_NAME = 'aikart-v1';
-const urlsToCache = [
-    './',
-    './index.html',
-    './style.css',
-    './script.js',
-    './manifest.json'
-];
+const CACHE_NAME = "alkart-v2026-NEW";
 
-self.addEventListener('install', event => {
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(cache => cache.addAll(urlsToCache))
-    );
+self.addEventListener("install", event => {
+  self.skipWaiting();
 });
 
-self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request)
-            .then(response => response || fetch(event.request))
-    );
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => caches.delete(key))
+      )
+    )
+  );
+  self.clients.claim();
 });
